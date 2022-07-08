453ee400148fb300175870dade97f63512567e62
Gegy <gegy.dev@gmail.com>
2022-07-08 12:28:32 +0200

    Initial commit: setup mod

---
+++ src/main/java/com/lovetropics/accessibility/LoveAccessibility.java
@@ -0,0 +1,20 @@
+package com.lovetropics.accessibility;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(LoveAccessibility.ID)
+public class LoveAccessibility {
+    public static final String ID = "love_accessibility";
+    public static final Logger LOGGER = LogManager.getLogger(ID);
+
+    public LoveAccessibility() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+}
